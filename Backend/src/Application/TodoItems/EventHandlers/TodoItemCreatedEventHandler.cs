28fcff44ba7a9681c18b75f01691a5f4c968cb12
menulk <menushilaksh5@gmail.com>
2023-07-19 09:22:30 +0530

    Backend setup version 7.0.2

---
+++ Backend/src/Application/TodoItems/EventHandlers/TodoItemCreatedEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using Backend.Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.Application.TodoItems.EventHandlers;
+
+public class TodoItemCreatedEventHandler : INotificationHandler<TodoItemCreatedEvent>
+{
+    private readonly ILogger<TodoItemCreatedEventHandler> _logger;
+
+    public TodoItemCreatedEventHandler(ILogger<TodoItemCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(TodoItemCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Backend Domain Event: {DomainEvent}", notification.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ Backend/src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Infrastructure.Identity;
+
+public class ApplicationUser : IdentityUser
+{
+}
