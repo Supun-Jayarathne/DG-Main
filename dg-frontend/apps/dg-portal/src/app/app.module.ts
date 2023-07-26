49fe7e53bfea10a28070a9d5279b23ef1ed2ab44
Supun Jayarathne <supun@bistecglobal.com>
2023-07-26 20:26:38 +0530

    link with frontend login2

--- dg-frontend/apps/dg-portal/src/app/app.module.ts
+++ dg-frontend/apps/dg-portal/src/app/app.module.ts
@@ -1,27 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { LoginComponent } from './Login/login.component';
-import { MenuComponent } from './Menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NxWelcomeComponent,
-    LoginComponent,
-    MenuComponent,
-  ],
+  declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    HttpClientModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-    FormsModule,
-    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- dg-frontend/apps/dg-portal/src/app/app.routes.ts
+++ dg-frontend/apps/dg-portal/src/app/app.routes.ts
@@ -1,4 +1,3 @@
 import { Route } from '@angular/router';
-import { LoginComponent } from './Login/login.component';
 
-export const appRoutes: Route[] = [{ path: 'login', component: LoginComponent }];
+export const appRoutes: Route[] = [];
