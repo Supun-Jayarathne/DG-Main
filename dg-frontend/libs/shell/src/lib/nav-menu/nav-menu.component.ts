e5453ebdad5a33109ec59f90d08bd66dd794f41b
menulk <menushilaksh5@gmail.com>
2023-07-26 19:30:29 +0530

    MaterialUI added , file paths added, Appcomponent , routes modifcations, Folder structure

---
+++ dg-frontend/libs/shell/src/lib/nav-menu/nav-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dg-frontend-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css'],
+})
+export class NavMenuComponent {}
