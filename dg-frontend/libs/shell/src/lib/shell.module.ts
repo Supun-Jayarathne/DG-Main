7a11c55cc6cf05b3583faf4a85aed4abccb1a8f2
Supun Jayarathne <supun@bistecglobal.com>
2023-07-28 12:23:06 +0530

    added route for dashboard and removed some unwanted codes

--- dg-frontend/libs/shell/src/lib/shell.module.ts
+++ dg-frontend/libs/shell/src/lib/shell.module.ts
@@ -17,8 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const appRoutes: Route[] = [
   { path: 'login', component: LoginComponent },
-  { path: '**', component: WelcomeComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: WelcomeComponent },
 ];
 
 @NgModule({
@@ -45,62 +45,3 @@ export const appRoutes: Route[] = [
   exports: [ NavMenuComponent, LoginComponent],
 })
 export class ShellModule {}
-
-
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { RouterModule } from '@angular/router';
-// import { AppComponent } from './app.component';
-// import { appRoutes } from './app.routes';
-// import { NxDashboardComponent } from './nx-dashboard.component';
-// import { DashboardComponent } from '../../../../libs/shell/src';
-// import {MatToolbarModule} from '@angular/material/toolbar';
-// import {MatIconModule} from '@angular/material/icon';
-// import {MatButtonModule} from '@angular/material/button';
-// import {MatSidenavModule} from '@angular/material/sidenav';
-// import { AppRoutingModule } from './app.routes';
-
-// //dashboard
-// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-// import { MatCardModule } from '@angular/material/card';
-// import { MatGridListModule } from '@angular/material/grid-list';
-// import { MatMenuModule } from '@angular/material/menu';
-// import {MatRadioModule} from '@angular/material/radio';
-
-// //Form
-// import {NgFor} from '@angular/common';
-// import {FormsModule} from '@angular/forms';
-
-// //Update Status
-// //import {MatFormFieldModule} from '@angular/material/form-field';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent, NxDashboardComponent, DashboardComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     MatToolbarModule,
-//     MatIconModule,
-//     MatButtonModule,
-//     MatSidenavModule,
-//     AppRoutingModule,
-//     //dashboard
-//     NoopAnimationsModule,
-//     MatCardModule,
-//     MatGridListModule,
-//     MatMenuModule,
-//     MatRadioModule,
-//     //Form
-//     FormsModule,
-//     NgFor,
-//     //Update Status
-//    // MatFormFieldModule,
-
-//     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent],
-// })
-// export class AppModule {}
-
