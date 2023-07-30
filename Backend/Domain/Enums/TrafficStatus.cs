fae0b83acb630612b319595380fdc9e5d615fbef
Supun Jayarathne <supun@bistecglobal.com>
2023-07-30 12:37:27 +0530

    merge main with client setup

---
+++ Backend/Domain/Enums/TrafficStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum TrafficStatus
+    {
+        GREEN = 1,
+        ORANGE = 2,
+        RED = 3
+    }
+}
